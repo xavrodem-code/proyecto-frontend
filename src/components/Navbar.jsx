7adf362035623fc4b78fbe373db082b0f8cfa764
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-04 20:54:44 +0100

    redirects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import "../components/Navbar.css"
-import {NavLink} from 'react-router-dom';
-
+import React from "react";
+import "../components/Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-        <ul className='navBar'>
-           <li>Inicio</li>
-           <li>Servicios</li>
-           <li>Acerca De</li>
-           <li>Contactos</li>
-        </ul>
+      <ul className="navBar">
+        <li>Inicio</li>
+        <li>Servicios</li>
+        <li>Acerca De</li>
+        <li>Contactos</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
