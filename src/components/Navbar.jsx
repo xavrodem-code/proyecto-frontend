4464a5bac665518d468973a2228d3d747e43ce9c
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-09 22:26:39 +0100

    lo del css de fechas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,6 @@ const Navbar = () => {
     <div>
       <ul className="navBar">
         <li>
-          <Link to="/" style={{ color: "black", textDecoration: "none" }}>
-            Inicio
-          </Link>
-        </li>
-        <li>
           <Link to="/login" style={{ color: "black", textDecoration: "none" }}>
             Ingresar
           </Link>
@@ -19,11 +14,6 @@ const Navbar = () => {
         <li>
           <Link to="/signup" style={{ color: "black", textDecoration: "none" }}>
             Registrarse
-          </Link>
-        </li>
-        <li>
-          <Link to="/fechas" style={{ color: "black", textDecoration: "none" }}>
-            Reservar Fechas
           </Link>
         </li>
       </ul>
--- src/pages/ReservarFechas.jsx
+++ src/pages/ReservarFechas.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import Fechas from "../components/Fechas";
-import { useNavigate } from "react-router-dom";
 import RegistrarFechas from "../components/RegistrarFechas";
 import "./ReservarFechas.css";
 
 const ReservarFechas = () => {
-  const usuario = JSON.parse(localStorage.getItem("datosUsuario"));
-  const navegar = useNavigate();
-  React.useEffect(() => {
-    if (!usuario || !usuario.token || !usuario.userId) {
-      navegar("/login");
-    }
-  }, [usuario, navegar]);
   return (
     <div className="reservar-fechas">
       <div className="fechas-container">
