38d38dda77e0b10f9cf3fa6cdb0ab2667399e6ff
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-09 21:25:14 +0100

    lo del css de fechas

--- src/pages/ReservarFechas.jsx
+++ src/pages/ReservarFechas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Fechas from "../components/Fechas";
-import { Redirect } from "react-router-dom";
+import { Navigate } from "react-router";
 import RegistrarFechas from "../components/RegistrarFechas";
 import "./ReservarFechas.css";
 
@@ -8,7 +8,7 @@ const ReservarFechas = () => {
   const usuario = JSON.parse(localStorage.getItem("datosUsuario"));
 
   if (!usuario || !usuario.token || !usuario.userId) {
-    return <Redirect to="/login" />;
+    return <Navigate to="/login" />;
   }
   return (
     <div className="reservar-fechas">
