f38adaf62d7a57ff1c50b7bbb0545e235e9a1e95
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-09 11:23:16 +0100

    lo del css de registrarfechas

--- src/components/RegistrarFechas.jsx
+++ src/components/RegistrarFechas.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import "../components/RegistrarFechas.css";
+
 const RegistrarFechas = () => {
   const [fecha, setFecha] = useState([""]);
 
