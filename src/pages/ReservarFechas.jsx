92e0f105fa591872aeda365e7598649bbbdb438f
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-09 21:04:07 +0100

    lo del css de fechas

--- src/pages/ReservarFechas.jsx
+++ src/pages/ReservarFechas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Fechas from "../components/Fechas";
-import { Redirect } from "react-router";
+import { Redirect } from "react-router-dom";
 import RegistrarFechas from "../components/RegistrarFechas";
 import "./ReservarFechas.css";
 
